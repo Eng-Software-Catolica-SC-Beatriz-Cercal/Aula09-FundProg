6021e296a204378dc80b819575216af495d1db4b
BeatrizCercal <biaccachoeira@gmail.com>
2026-05-02 22:08:47 -0300

    exercicio 10 finalizado

--- exercicio10.c
+++ exercicio10.c
@@ -1,3 +1,29 @@
-//Construa um programa que leia uma matriz 4x4 e conte quantos
-//elementos são maiores que 10. Ao final, exiba a quantidade
-//encontrada.
+#include <stdio.h>
+
+int main(void){
+    // variáveis
+    int i, j;
+    int matriz [4][4];
+    int contador = 0;
+
+
+    // entrada de dados
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 4; j++){
+            printf("Digite um valor: ");
+            scanf ("%i", &matriz[i][j]);
+            fflush(stdin);
+
+        }
+    }
+    //processamento e saida
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 4; j++){
+            if(matriz[i][j] > 10){
+                contador++;
+            }
+        }
+    }
+    printf("Quantidade de elementos maiores que 10: %d", contador);
+    return 0;
+}
